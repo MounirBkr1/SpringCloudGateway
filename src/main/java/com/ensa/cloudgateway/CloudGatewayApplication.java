c277b111bceb8e0b6ffb8894f2c6a3ec7a6df507
Mounir Bkr <miirbri1@gmail.com>
2023-02-02 15:46:46 +0100

    commit0 "final"

--- src/main/java/com/ensa/cloudgateway/CloudGatewayApplication.java
+++ src/main/java/com/ensa/cloudgateway/CloudGatewayApplication.java
@@ -1,5 +1,7 @@
 package com.ensa.cloudgateway;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
@@ -7,7 +9,13 @@ import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitio
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.context.annotation.Bean;
 
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
 @SpringBootApplication
+@OpenAPIDefinition(info=@Info(title="gateway",version="1.0",description="description of my open api for gateway"))
 public class CloudGatewayApplication {
 
     public static void main(String[] args) {
@@ -32,5 +40,22 @@ public class CloudGatewayApplication {
     DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp){
         //rdc:reactive discovery client, dlp: discovery locator properties
         return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
+    }
+
+
+    @Bean
+    CorsWebFilter corsFilter() {
+        return new CorsWebFilter(corsConfigurationSource());
+    }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:4200");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
